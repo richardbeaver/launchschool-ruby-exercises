9081c87d506240982624f011c493886f96673dd6
Richard Beaver <beaver.r@northeastern.edu>
2023-08-08 21:22:18 -0400

    Add explanation of negating negative input value

--- rb101_rb119_small_problems/easy_4/e4_10.rb
+++ rb101_rb119_small_problems/easy_4/e4_10.rb
@@ -11,7 +11,26 @@
 # such as Integer#to_s, String(), Kernel#format, etc. You may, however, use
 # `integer_to_string` from the previous exercise.
 
+# Examples
+p signed_integer_to_string(4321) == '+4321'
+p signed_integer_to_string(-123) == '-123'
+p signed_integer_to_string(0) == '0'
+
 # ==============================================
+
+=begin
+
+We want to be sure we are not performing modulo operations involving negative
+numbers, as this will cause different results than is expected with only
+non-negative numbers.
+
+This is why, when the input is negative, we want to pass the positive
+of the value to the `integer_to_string` helper method, which uses modulo.
+
+In our second test case, if we do not negate that negative input before
+passing it to `integer_to_string`, the loop becomes an infinite loop.
+
+=end
 
 DIGITS = %w[0 1 2 3 4 5 6 7 8 9].freeze
 
@@ -36,8 +55,3 @@ def integer_to_string(number)
 
   result
 end
-
-# Examples
-p signed_integer_to_string(4321) == '+4321'
-p signed_integer_to_string(-123) == '-123'
-p signed_integer_to_string(0) == '0'
